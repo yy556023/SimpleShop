562b71344883e5ccc5f5454b55e558b6b7e86c76
Yuu <yy556023@gmail.com>
2023-09-04 20:02:36 +0800

    更新 註冊服務

--- aspnet-core/src/SimpleShop.HttpApi.Host/Program.cs
+++ aspnet-core/src/SimpleShop.HttpApi.Host/Program.cs
@@ -1,4 +1,13 @@
 
+using SimpleShop.Application.Goods;
+using SimpleShop.Application.Contracts.Goods;
+using SimpleShop.Domain.Goods;
+using SimpleShop.EntityFrameworkCore.Goods;
+using SimpleShop.Application.Contracts.Categories;
+using SimpleShop.Application.Categories;
+using SimpleShop.Domain.Categories;
+using SimpleShop.EntityFrameworkCore.Categories;
+
 namespace SimpleShop.HttpApi.Host
 {
     public class Program
@@ -8,6 +17,22 @@ namespace SimpleShop.HttpApi.Host
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            // AppService
+            builder.Services.AddScoped<IGoodAppService, GoodAppService>();
+            builder.Services.AddScoped<ICategoryAppService, CategoryAppService>();
+
+            // Repository
+            builder.Services.AddScoped<IGoodRepository, GoodRepository>();
+            builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+            // Manager
+            builder.Services.AddScoped<GoodManager>();
+            builder.Services.AddScoped<CategoryManager>();
+
+            // Adapter
+            builder.Services.AddScoped<GoodAdapter>();
+            builder.Services.AddScoped<CategoryAdapter>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
